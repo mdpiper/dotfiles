7220a15743d49e07dde2f17e94570bac7117313c
Mark Piper <mark.piper@colorado.edu>
2015-09-06 20:13:38 -0600

    Add git config script

---
+++ git_setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git config --global user.name "Mark Piper"
+git config --global user.email mark.piper@colorado.edu
+git config --global core.editor "emacs -nw"
+git config --global color.ui true
